0fce6e39f0710dff1d667ab24cc5eaa42afa6bca
S1ntax3rror <jiri@nxu.ch>
2026-05-13 14:56:44 +0200

    merged xpu array and makie version, updated runfile, removed 3D runfile

--- run_files/l8_2D_runner.sh
+++ run_files/l8_2D_runner.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#SBATCH --account class04
+#SBATCH --account=julia-gpu-course2026-ethz
 #SBATCH --job-name="convect2D"
 #SBATCH --output=convect2D.%j.o
 #SBATCH --error=convect2D.%j.e
@@ -8,4 +8,4 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-task=1
 
-srun --uenv julia/25.5:v1 --view=juliaup julia --project PorousConvection_2D_xpu.jl
+srun --uenv julia/25.5:v1 --view=juliaup julia --project ../src/xpu/2d_swe_xpu.jl
