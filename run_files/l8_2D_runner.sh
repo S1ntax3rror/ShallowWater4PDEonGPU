d5315ce008e8ad5cff0844ca38d875a689b8fab3
S1ntax3rror <jiri@nxu.ch>
2026-05-13 15:36:36 +0200

    update runner

---
+++ run_files/l8_2D_runner.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+#SBATCH --account=julia-gpu-course2026-ethz
+#SBATCH --job-name="convect2D"
+#SBATCH --output=convect2D.%j.o
+#SBATCH --error=convect2D.%j.e
+#SBATCH --time=03:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-task=1
+
+srun --uenv julia/25.5:v1 --view=juliaup julia --project src/xpu/2d_swe_xpu.jl
